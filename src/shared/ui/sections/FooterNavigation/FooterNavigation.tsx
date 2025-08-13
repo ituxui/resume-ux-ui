8a9b56d6de10b822329a6ad3ddc2c58ec42f8ec3
ituxui <yuriymlm@gmail.com>
2025-08-13 16:57:36 +1000

    first commit

--- src/shared/ui/sections/FooterNavigation/FooterNavigation.tsx
+++ src/shared/ui/sections/FooterNavigation/FooterNavigation.tsx
@@ -25,10 +25,10 @@ export const FooterNavigation: React.FC<PropsFooterNavigation> = ({ current }) =
         <Link to={`../${next.url}`} className={classNames(styles.navButton, styles.next)}>
           <span className={styles.secondary}>Следующий проект</span>
           <div className={styles.buttonContent}>
+            {next.title}
             <svg className={styles.icon} width="24" height="24" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
               <path d="M9 18L15 12L9 6" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round" />
             </svg>
-            {next.title}
           </div>
         </Link>
       )}
